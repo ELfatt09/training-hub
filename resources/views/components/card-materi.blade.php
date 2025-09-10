495786c3dfb39af118661c0b3c34da9e22fbaaa0
Consomeatsky <ibnulaginugas@gmail.com>
2025-09-10 08:13:39 +0700

    fixed card-materi

--- resources/views/components/card-materi.blade.php
+++ resources/views/components/card-materi.blade.php
@@ -1,43 +1,39 @@
-<div id="accordion-open" data-accordion="open" class="w-full mx-auto mt-6">
-    <div id="accordion-open-heading-{{ $accordionid }} ">
-        <button type="button" 
-            class="flex items-center justify-between w-full p-4 font-medium text-left text-gray-700 bg-white border-y-darkoff border"
-            data-accordion-target="#accordion-open-body-1" 
-            aria-expanded="true" 
-            aria-controls="accordion-open-body-1">
-            
-            {{-- Title & Description --}}
-            <div class="flex items-center gap-3">
-                <div class="w-8 h-8 flex items-center justify-center rounded-full bg-teal-500 text-white font-bold">
-                    ✓
-                </div>
-                <div>
-                    {{-- Title --}}
-                    <div class="text-xl">
-                        <h1>{{ $title }}</h1>
+<div id="accordion-open-{{ $accordionid }}" data-accordion="open" class="w-full mx-auto mt-6">
+    @foreach ($materi as $index => $item)
+        <div id="accordion-open-heading-{{ $accordionid }}-{{ $index }}">
+            <button type="button" 
+                class="flex items-center justify-between w-full p-4 font-medium text-left text-gray-700 bg-white border-y-darkoff border"
+                data-accordion-target="#accordion-open-body-{{ $accordionid }}-{{ $index }}" 
+                aria-expanded="false" 
+                aria-controls="accordion-open-body-{{ $accordionid }}-{{ $index }}">
+                
+                {{-- Title & Description --}}
+                <div class="flex items-center gap-3">
+                    <div class="w-8 h-8 flex items-center justify-center rounded-full bg-teal-500 text-white font-bold">
+                        ✓
                     </div>
-                    {{-- Description --}}
-                    <div class="text-xs flex gap-4">
-                        <h4><i class="fa-solid fa-bookmark"></i> {{ $bab }}</h4>
+                    <div>
+                        <div class="text-xl">
+                            <h1>{{ $title }}</h1>
+                        </div>
+                        <div class="text-xs flex gap-4">
+                            <h4><i class="fa-solid fa-bookmark"></i> {{ $bab }}</h4>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <svg data-accordion-icon class="w-5 h-5 shrink-0 transition-transform" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7"/>
-            </svg>
-        </button>
-    </div>
-        @foreach ($materi as $item)
-    <a href="" id="accordion-open-body-1" class="hidden" aria-labelledby="accordion-open-heading-{{ $accordionid }}">
-
-             <div class="p-4 border border-t-0 rounded-b-lg bg-milk text-sm text-gray-600 flex justify-between">
-            <div class="space-y-2">
-                <h1><i class="fa-solid fa-books mr-1"></i> {{ $item->title }}</h1>
-                <h1><i class="fa-regular fa-messages"></i> Pembahasan Dasar</h1>
-            </div>
+                <svg data-accordion-icon class="w-5 h-5 shrink-0 transition-transform" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7"/>
+                </svg>
+            </button>
         </div>
 
-    </a>
-            @endforeach
+        <div id="accordion-open-body-{{ $accordionid }}-{{ $index }}" class="hidden" aria-labelledby="accordion-open-heading-{{ $accordionid }}-{{ $index }}">
+            <div class="p-4 border border-t-0 rounded-b-lg bg-milk text-sm text-gray-600 flex justify-between">
+                @foreach ($ as $item)
+                    
+                @endforeach
+            </div>
+        </div>
+    @endforeach
 </div>
